6906530516f2fe35de082fb75d6854a48ef6be7b
Teller501 <113039777+Teller501@users.noreply.github.com>
2024-02-05 09:39:01 +0000

    completed list1.py

--- ses2/exercises/list1.py
+++ ses2/exercises/list1.py
@@ -18,7 +18,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
     # +++your code here+++
-    return 
+    count = 0
+    for i in words:
+        if len(i) >= 2 and i[0] == i[-1]:
+            count += 1
+    return count
 
 
 # B. front_x
@@ -29,9 +33,18 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return 
+    x_list = []
+    words_list = []
+    for i in words:
+        if i[0] == 'x':
+            x_list.append(i)
+        else:
+            words_list.append(i)
 
+    words_list.sort()
+    x_list.sort()
+
+    return x_list+words_list
 
 
 # C. sort_last
@@ -41,48 +54,48 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 
-
+def get_last_element(tuple):
+    return tuple[-1]
 
 def sort_last(tuples):
     # +++your code here+++
-    return 
+    return sorted(tuples, key=get_last_element)
 
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print (f'{prefix} got: {got} expected: {expected}')
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print(f'{prefix} got: {got} expected: {expected}')
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print ('match_ends')
-  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
-  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
-  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+    print('match_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
-  print()
-  print ('front_x')
-  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
-       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
-       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
-       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+    print()
+    print('front_x')
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
-  print()
-  print ('sort_last')
-  test(sort_last([(1, 3), (3, 2), (2, 1)]),
-       [(2, 1), (3, 2), (1, 3)])
-  test(sort_last([(2, 3), (1, 2), (3, 1)]),
-       [(3, 1), (1, 2), (2, 3)])
-  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
-       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    print()
+    print('sort_last')
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
 
 
 main()
---
+++ ses2/in_class.py
@@ -0,0 +1,14 @@
+# Exercise 0.1 - Slicing
+sentence = ['Hello', 'World', 'Huston', 'we', 'are', 'here']
+
+print(sentence[1:5])
+print(sentence[0:2])
+print(sentence[-2:])
+print(sentence[-2:-1])
+print(sentence[::2])
+print(sentence[::-1])
+print(sentence[1:-1])
+print(sentence[1:4])
+
+# Exercise 6 - Storebælt
+car1 = {'DD12312': {'car_color': 'blue', 'passengers': 2, 'car_length': '3,2m'}}
