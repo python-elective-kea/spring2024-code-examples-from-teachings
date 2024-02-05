27a40d27e225c4985d89b5c3da0160bf08aa6879
clbokea <clbo@kea.dk>
2024-02-05 09:31:22 +0100

    pause

---
+++ ses2/code_from_teachings.py
@@ -0,0 +1,47 @@
+# list
+names = ['Claus', 'Henning', 'Tove', 'Tim']
+
+# Definition
+
+# refere
+#print(names[2])
+
+# Add or change Values
+#names[0] = 'Torben'
+
+# Build in functions 
+#print(len(names))
+#print( tuple(names) )
+
+
+# Concatenate
+
+# print(names * 10)
+
+# Slicing
+
+#print( names[-1] )
+
+# for each loop
+"""
+for i in range(2,10, 2):
+    print(i)
+"""
+
+print( names.index('Tove')  )
+
+
+
+
+
+
+"""
+age_list = [53]
+t = ('Claus', age_list, 'male', 'clbo@kea.dk', '310119701313')
+#t[0] = 'Torben'
+print(t)
+
+age_list[0] = 46
+
+print(t)
+""" 
