6fb92af4dee1b4e78c641add1d4d88bb40e605ba
F4MES <tobias.schwartzlose@gmail.com>
2024-03-04 09:23:11 +0100

    hi

--- SoupParse/SoupParseJonindex.py
+++ SoupParse/SoupParseJonindex.py
@@ -18,4 +18,8 @@ for job in job_listings:
     title = job.find('h4').text.strip()
     company = job.find('div', class_='jix-toolbar-top__company').text.strip()
     location = job.find('span', class_='jix_robotjob--area').text.strip()
+    print('-----------------------------------------------------')
+    print('')
     print(f"Job Title: {title}\nCompany: {company}\nLocation: {location}\n")
+
+    
