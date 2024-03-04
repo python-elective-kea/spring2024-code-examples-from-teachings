bdb9f305f9f0b4f4802b776750b613d860e2e080
clbokea <clbo@kea.dk>
2024-03-04 09:39:38 +0100

    dec

--- ses6/decorators.py
+++ ses6/decorators.py
@@ -1,15 +1,39 @@
 # First class functions (objects)
-def f1(parameter):
-    pass
+
+def f1(p):
+    return p
+
+
+
+def foo():
+    print('Hello from foo')
+
 
 
 # Inner functions
 def foo():
-    pass
+    
     # define
-
+    def bar():
+        return 'From bar'
+    
     #execute
-
-
+    return bar()
 
 # Decorators
+
+def addtekst(func):
+    def wrapper(*args):
+        print('Summen af de to tal: ')
+        func(*args)
+    return wrapper
+
+@addtekst
+def add(n1, n2):
+    print(n1 + n2)
+
+
+# add = addtekst(add)
+
+
+
