704adf7b9db36f35dd86f52a6914ff5808c7a210
clbokea <clbo@kea.dk>
2024-03-18 09:46:35 +0100

    deck start

---
+++ ses9/deck.py
@@ -0,0 +1,9 @@
+class Deck:
+    def __init__(self):
+        self.cards = ['A', 'K', 4, 7]
+
+    def __repr__(self):
+        return f'{self.__dict__}'
+
+    def __getitem__(self, key):
+        return self.cards[key]
