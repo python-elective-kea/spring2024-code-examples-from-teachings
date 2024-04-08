d1d9c5944ab529c02cd15160c0b8d3458d44e56b
clbokea <clbo@kea.dk>
2024-04-08 08:11:44 +0200

    ses10 start

---
+++ ses10/prop.py
@@ -0,0 +1,17 @@
+# prop.py
+
+# In python everything is public
+
+class Person:
+   pass 
+
+
+
+
+# We have looked at:
+# * instance variable
+# * Class Variables
+# * methods
+#   * datammodel methods
+
+# Now we look at properties
